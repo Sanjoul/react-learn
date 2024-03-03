1d75da87ba918815b5bffdebcb8973b2bcaf8c6c
Sanjoul Bhusal <bsanjoul@gmail.com>
2024-03-03 11:27:18 +0545

    LEARNED_USECALLBACK

--- src/components/GenerateRandomNumber.jsx
+++ src/components/GenerateRandomNumber.jsx
@@ -1,3 +1,7 @@
+//Memo is a higher-order component(HOC) that memoizes the rendering of a function component.
+//Higher-order components (HOCs) are a powerful feature of the React library. They allow you to reuse component logic across multiple components. In React, a higher-order component is a function that takes a component as an argument and returns a new component that wraps the original component.
+//It prevent the functional component from re-rendering if its props haven't changed.
+//IT's particularly useful when you have componentts that are rendering with the same props but don't need to update when those props change
 import {memo} from "react"
 
 export const GenerateRandomNumber = memo(({
@@ -10,4 +14,4 @@ export const GenerateRandomNumber = memo(({
         <button onClick={generateRandomNumber}> Generate Randome Number</button>
         </>
     )
-})  
+})   
--- src/components/LearnUseCallback.jsx
+++ src/components/LearnUseCallback.jsx
@@ -1,7 +1,23 @@
 //This component is acting like parent component for GenerateRandomNumber
+//If parent re-render it is not necessary to render child
+import { useCallback,useState } from "react";
+import { GenerateRandomNumber } from "./GenerateRandomNumber";
+
 export const LearnUseCallback = () => {
-  return (
+    const [count,setCount]=useState(0)
+    const handleIncreaseCount = () =>{
+        setCount((prevCount)=>prevCount +1)
+    }
+    const[randomNumber,setRandomNumber]=useState(0);
+    const generateRandomNumber =()=>{
+        const newRandomNumber = Math.floor(Math.random()*100)+1;
+        setRandomNumber(newRandomNumber);
+    }
+    return (
     <>
+    <GenerateRandomNumber randomNumber={randomNumber} generateRandomNumber={generateRandomNumber}/>
+    <h1>Count: {count}</h1>
+    <button onClick={handleIncreaseCount}>Increase Count</button>
     </>
   )
 }
